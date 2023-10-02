63a5792309344d856445cc386fe3c5690474ae76
Miguel Martinez <mike280715@gmail.com>
2023-10-01 23:28:04 -0600

    cambio de posicion de imagenes

--- src/components/WeatherContainer.jsx
+++ src/components/WeatherContainer.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import WeatherStat from "./WeatherStat";
-import weatherIcons from "../utils/weatherIcons";
 
 
 const WeatherContainer = ({ weather }) => {
@@ -19,6 +18,17 @@ const WeatherContainer = ({ weather }) => {
   const handleChangeUnit = () => {
     setIsCelsius(!isCelsius);
   };
+
+  const weatherIcons = {
+    "Drizzle": "/public/img/rain.svg",
+    "Thunderstorm": "/public/img/thunderStorm.svg",
+    "Clear": "/public/img/clearSky.svg",
+    "Clouds": "/public/img/fewClouds.svg",
+    "Mist": "/public/img/mist.svg",
+    "Rain": "/public/img/showerRain.svg",
+    "Snow": "/public/img/snow.svg",
+    
+  }
 
   
   return (
