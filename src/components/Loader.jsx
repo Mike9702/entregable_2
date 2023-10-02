b78c2c977ac4651e66890689182b08950cbd6715
Miguel Martinez <mike280715@gmail.com>
2023-10-01 23:35:14 -0600

    modificado2

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <section className="grid grid-cols-1 gap-2 justify-items-center">
-     <img src="/public/img/weather.svg" alt="" />
+     <img src="/public/img/weather.png" alt="" />
      <h2>Weather App</h2>
     <div className="loader"></div>
     </section>
